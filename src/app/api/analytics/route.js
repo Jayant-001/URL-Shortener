7a845955610021a918577418bf7f634d03b3d1f9
Jayant-001 <jayant.gupta.dln@gmail.com>
2023-10-25 13:33:25 +0530

    created analytics route

--- src/app/api/analytics/route.js
+++ src/app/api/analytics/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const GET = async (req) => {
     try {
 
-        
+        return NextResponse.json({ message: "Analytics route" }, { status: 200 });
 
     } catch (error) {
         return NextResponse.json({ error: error.message }, { status: 500 });
